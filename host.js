6092e9ceb556433aee36dddc94c232b371740a07
Rhys Lohf <rhys.lohf@gmail.com>
2018-06-25 11:52:42 +0930

    Added sharp dependency to do compression/rotating of images. fixed issue with badly orientated

--- host.js
+++ host.js
@@ -3,6 +3,8 @@ const multer  = require('multer')
 const app = express()
 const path = require('path')
 
+const sharp = require('sharp');
+
 const request = require('request')
 const imagemin = require('imagemin')
 const imageminPngquant = require('imagemin-pngquant')
@@ -13,9 +15,11 @@ var plateToPlateInformation = require('./ezyreg.js')
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/', express.static(path.join(__dirname, 'static')))
 
+
+/*
 var compressImageBuffer = function(buffer, quality) {
     console.log("Compressing Image Buffer")
-    console.log("Buffer size "+buffer.length/1024+"kb")
+    console.log("Buffer size "+Math.round(buffer.length/1024)+"kb")
     return new Promise(function(resolve, reject) {
         imagemin.buffer(buffer, {plugins: [imageminPngquant({quality:quality})]})
             .then(function(buffer) {
@@ -28,10 +32,29 @@ var compressImageBuffer = function(buffer, quality) {
             })
     })
 }
+*/
+
+var compressImageBuffer = function(buffer) {
+    console.log("Compressing Image Buffer")
+    console.log("Buffer size "+Math.round(buffer.length/1024)+"kb")
+    return new Promise(function(resolve, reject) {
+        sharp(buffer)
+            .resize(1024) // wide
+            .rotate()
+            .jpeg({quality:80})
+            .toBuffer()
+            .then(function(resizedBuffer) {
+                resolve(resizedBuffer)
+            })
+            .catch(function(error) {
+                reject(error)
+            })
+    })
+}
 
 var imageBufferToBase64 = function(buffer) {
     console.log("Converting Image Buffer to Base64")
-    console.log("Buffer size "+buffer.length/1024+"kb")
+    console.log("Buffer size "+Math.round(buffer.length/1024)+"kb")
     return new Promise(function(resolve, reject) {
         try {
             var base64 = buffer.toString("base64")
@@ -46,9 +69,10 @@ var imageBufferToBase64 = function(buffer) {
 
 var base64ToPlate = function(base64) {
     console.log("Converting Base64 Image to Plate Recognition")
-    console.log(base64)
+    console.log("Base64 size "+Math.round(base64.length/1333)+"kb")
+
     return new Promise(function(resolve, reject) {
-        var secret_key = process.env.SECRET_KEY || '';
+        var secret_key = process.env.SECRET_KEY || 'sk_8af55fbf65cf9bcefd219b6a';
         var url = 'https://api.openalpr.com/v2/recognize_bytes?limit=1&recognize_vehicle=0&country=au&secret_key=';
         request.post({'url':url + secret_key, body: base64},
             function(error, response, body) {
